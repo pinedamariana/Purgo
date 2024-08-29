0c0d66b3fd344cab371a1460ea56da70dd744e95
Purgo AI <support@purgo.ai>
2024-08-29 10:40:22 -0500

    Commit message

--- scan-databricks/table/OrderDetails.Table.sql
+++ scan-databricks/table/OrderDetails.Table.sql
@@ -1,11 +1,15 @@
 
-CREATE TABLE `Order_Details` (
-  `OrderID` INTEGER NOT NULL,
-  `ProductID` INTEGER NOT NULL,
-  `UnitPrice` DECIMAL(19,4) NOT NULL DEFAULT 0,
-  `Quantity` SMALLINT NOT NULL DEFAULT 1,
-  `Discount` FLOAT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`OrderID`, `ProductID`),
-  FOREIGN KEY (`OrderID`) REFERENCES `Orders`(`OrderID`),
-  FOREIGN KEY (`ProductID`) REFERENCES `Products`(`ProductID`)
-)
+CREATE TABLE `Order Details` (
+    `OrderID` INT NOT NULL, 
+    `ProductID` INT NOT NULL, 
+    `UnitPrice` DECIMAL(19,4) NOT NULL DEFAULT 0, 
+    `Quantity` SMALLINT NOT NULL DEFAULT 1, 
+    `Discount` FLOAT NOT NULL DEFAULT 0,
+    PRIMARY KEY (`OrderID`, `ProductID`)
+);
+
+ALTER TABLE `Order Details` 
+    ADD CONSTRAINT `FK_Order_Details_Orders` FOREIGN KEY (`OrderID`) REFERENCES `Orders`(`OrderID`);
+
+ALTER TABLE `Order Details` 
+    ADD CONSTRAINT `FK_Order_Details_Products` FOREIGN KEY (`ProductID`) REFERENCES `Products`(`ProductID`);
