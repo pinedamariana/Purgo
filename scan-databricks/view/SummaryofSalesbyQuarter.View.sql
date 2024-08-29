e118fda3d879a5ea73183288d7bf84864d841019
Purgo AI <support@purgo.ai>
2024-08-28 19:31:18 -0500

    Commit message

---
+++ scan-databricks/view/SummaryofSalesbyQuarter.View.sql
@@ -0,0 +1,7 @@
+
+CREATE VIEW `Summary of Sales by Quarter` AS
+SELECT Orders.ShippedDate, Orders.OrderID, `Order Subtotals`.Subtotal
+FROM Orders 
+INNER JOIN `Order Subtotals` ON Orders.OrderID = `Order Subtotals`.OrderID
+WHERE Orders.ShippedDate IS NOT NULL
+
