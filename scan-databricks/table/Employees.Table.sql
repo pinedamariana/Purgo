4209fd89d2cc395018bc3f415565e0a527262723
Purgo AI <support@purgo.ai>
2024-08-29 10:40:15 -0500

    Commit message

--- scan-databricks/table/Employees.Table.sql
+++ scan-databricks/table/Employees.Table.sql
@@ -1,23 +1,24 @@
 
-CREATE TABLE `Employees` (
-  `EmployeeID` INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-  `LastName` STRING(20) NOT NULL,
-  `FirstName` STRING(10) NOT NULL,
-  `Title` STRING(30),
-  `TitleOfCourtesy` STRING(25),
-  `BirthDate` TIMESTAMP,
-  `HireDate` TIMESTAMP,
-  `Address` STRING(60),
-  `City` STRING(15),
-  `Region` STRING(15),
-  `PostalCode` STRING(10),
-  `Country` STRING(15),
-  `HomePhone` STRING(24),
-  `Extension` STRING(4),
-  `Photo` BINARY,
-  `Notes` STRING,
-  `ReportsTo` INT,
-  `PhotoPath` STRING(255),
-  PRIMARY KEY (`EmployeeID`),
-  FOREIGN KEY (`ReportsTo`) REFERENCES `Employees` (`EmployeeID`)
-)
+CREATE TABLE Employees (
+    EmployeeID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, 
+    LastName STRING NOT NULL, 
+    FirstName STRING NOT NULL, 
+    Title STRING NULL, 
+    TitleOfCourtesy STRING NULL, 
+    BirthDate TIMESTAMP NULL, 
+    HireDate TIMESTAMP NULL, 
+    Address STRING NULL, 
+    City STRING NULL, 
+    Region STRING NULL, 
+    PostalCode STRING NULL, 
+    Country STRING NULL, 
+    HomePhone STRING NULL, 
+    Extension STRING NULL, 
+    Photo BINARY NULL, 
+    Notes STRING NULL, 
+    ReportsTo INTEGER NULL, 
+    PhotoPath STRING NULL
+);
+
+ALTER TABLE Employees ADD CONSTRAINT PK_Employees PRIMARY KEY (EmployeeID);
+ALTER TABLE Employees ADD CONSTRAINT FK_Employees_Employees FOREIGN KEY (ReportsTo) REFERENCES Employees (EmployeeID);
