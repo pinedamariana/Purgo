fb070de96b6c8388f86c8b4af0662733e8a87e0e
Purgo AI <support@purgo.ai>
2024-08-28 19:04:50 -0500

    Commit message

---
+++ scan/table/CustomerCustomerDemo.Table.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE [CustomerCustomerDemo] (
+	[CustomerID] NCHAR(5) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL, 
+	[CustomerTypeID] NCHAR(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL, 
+	CONSTRAINT [PK_CustomerCustomerDemo] PRIMARY KEY ([CustomerID], [CustomerTypeID]), 
+	CONSTRAINT [FK_CustomerCustomerDemo] FOREIGN KEY([CustomerTypeID]) REFERENCES [CustomerDemographics] ([CustomerTypeID]), 
+	CONSTRAINT [FK_CustomerCustomerDemo_Customers] FOREIGN KEY([CustomerID]) REFERENCES [Customers] ([CustomerID])
+)
+
