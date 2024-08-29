2e02e2785c10957fa271b0b519203f74dff2f6a5
Purgo AI <support@purgo.ai>
2024-08-29 10:41:05 -0500

    Commit message

--- scan-databricks/view/ProductsbyCatery.View.sql
+++ scan-databricks/view/ProductsbyCatery.View.sql
@@ -1,17 +1,16 @@
 
-CREATE OR REPLACE VIEW `Products by Catery` AS
-SELECT 
-    Cateries.CateryName, 
-    Products.ProductName, 
-    Products.QuantityPerUnit, 
-    Products.UnitsInStock, 
-    Products.Discontinued
-FROM 
-    Cateries 
-INNER JOIN 
-    Products 
-ON 
-    Cateries.CateryID = Products.CateryID
-WHERE 
-    Products.Discontinued <> 1
--- ORDER BY Cateries.CateryName, Products.ProductName
+CREATE VIEW `Products by Catery` AS
+SELECT
+  Cateries.CateryName,
+  Products.ProductName,
+  Products.QuantityPerUnit,
+  Products.UnitsInStock,
+  Products.Discontinued
+FROM
+  Cateries
+INNER JOIN
+  Products
+ON
+  Cateries.CateryID = Products.CateryID
+WHERE
+  Products.Discontinued <> 1;
