d1cf3ef3adb5998c567191bb98d00233f8f60578
Purgo AI <support@purgo.ai>
2024-08-29 10:40:49 -0500

    Commit message

--- scan-databricks/view/Invoices.View.sql
+++ scan-databricks/view/Invoices.View.sql
@@ -1,33 +1,19 @@
 
-CREATE VIEW Invoices AS
-SELECT 
-    Orders.ShipName, 
-    Orders.ShipAddress, 
-    Orders.ShipCity, 
-    Orders.ShipRegion, 
-    Orders.ShipPostalCode, 
-    Orders.ShipCountry, 
-    Orders.CustomerID, 
-    Customers.CompanyName AS CustomerName, 
-    Customers.Address, 
-    Customers.City, 
-    Customers.Region, 
-    Customers.PostalCode, 
-    Customers.Country, 
-    CONCAT(FirstName, ' ', LastName) AS Salesperson, 
-    Orders.OrderID, 
-    Orders.OrderDate, 
-    Orders.RequiredDate, 
-    Orders.ShippedDate, 
-    Shippers.CompanyName AS ShipperName, 
-    `Order Details`.ProductID, 
-    Products.ProductName, 
-    `Order Details`.UnitPrice, 
-    `Order Details`.Quantity, 
-    `Order Details`.Discount, 
-    CAST((`Order Details`.UnitPrice * `Order Details`.Quantity * (1 - `Order Details`.Discount)) AS DECIMAL(19, 4)) AS ExtendedPrice, 
+CREATE OR REPLACE VIEW Invoices AS
+SELECT Orders.ShipName, Orders.ShipAddress, Orders.ShipCity,
+    Orders.ShipRegion, Orders.ShipPostalCode, Orders.ShipCountry,
+    Orders.CustomerID, Customers.CompanyName AS CustomerName, 
+    Customers.Address, Customers.City, Customers.Region, 
+    Customers.PostalCode, Customers.Country, 
+    CONCAT(Employees.FirstName, ' ', Employees.LastName) AS Salesperson, 
+    Orders.OrderID, Orders.OrderDate, Orders.RequiredDate, 
+    Orders.ShippedDate, Shippers.CompanyName AS ShipperName, 
+    "Order Details".ProductID, Products.ProductName, 
+    "Order Details".UnitPrice, "Order Details".Quantity, 
+    "Order Details".Discount, 
+    (CAST("Order Details".UnitPrice * "Order Details".Quantity * (1 - "Order Details".Discount) AS MONEY)) AS ExtendedPrice, 
     Orders.Freight
-FROM Shippers 
+FROM Shippers
 INNER JOIN (
     Products 
     INNER JOIN (
@@ -35,7 +21,10 @@ INNER JOIN (
         INNER JOIN (
             Customers 
             INNER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
-        ) ON Employees.EmployeeID = Orders.EmployeeID
-        INNER JOIN `Order Details` ON Orders.OrderID = `Order Details`.OrderID
-    ) ON Products.ProductID = `Order Details`.ProductID
-) ON Shippers.ShipperID = Orders.ShipVia;
+        )
+        ON Employees.EmployeeID = Orders.EmployeeID
+        INNER JOIN "Order Details" ON Orders.OrderID = "Order Details".OrderID
+    )
+    ON Products.ProductID = "Order Details".ProductID
+)
+ON Shippers.ShipperID = Orders.ShipVia;
