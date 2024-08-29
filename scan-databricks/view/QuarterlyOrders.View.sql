ed7e911a5597921cef210135e15722bdc480c63e
Purgo AI <support@purgo.ai>
2024-08-28 19:31:12 -0500

    Commit message

---
+++ scan-databricks/view/QuarterlyOrders.View.sql
@@ -0,0 +1,5 @@
+
+CREATE VIEW `Quarterly Orders` AS
+SELECT DISTINCT Customers.CustomerID, Customers.CompanyName, Customers.City, Customers.Country
+FROM Customers RIGHT OUTER JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+WHERE Orders.OrderDate BETWEEN '1997-01-01' AND '1997-12-31';
