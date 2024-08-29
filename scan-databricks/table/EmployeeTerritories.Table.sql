048b565c9369bad437d1456e21c2d6e364724fef
Purgo AI <support@purgo.ai>
2024-08-29 10:40:19 -0500

    Commit message

--- scan-databricks/table/EmployeeTerritories.Table.sql
+++ scan-databricks/table/EmployeeTerritories.Table.sql
@@ -1,8 +1,9 @@
 
 CREATE TABLE EmployeeTerritories (
-    EmployeeID INTEGER NOT NULL, 
-    TerritoryID STRING NOT NULL, 
-    PRIMARY KEY (EmployeeID, TerritoryID),
-    FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID), 
-    FOREIGN KEY (TerritoryID) REFERENCES Territories (TerritoryID)
-) USING DELTA;
+  EmployeeID INTEGER NOT NULL,
+  TerritoryID STRING NOT NULL,
+  PRIMARY KEY (EmployeeID, TerritoryID)
+);
+
+ALTER TABLE EmployeeTerritories ADD CONSTRAINT FK_EmployeeTerritories_Employees FOREIGN KEY (EmployeeID) REFERENCES Employees (EmployeeID);
+ALTER TABLE EmployeeTerritories ADD CONSTRAINT FK_EmployeeTerritories_Territories FOREIGN KEY (TerritoryID) REFERENCES Territories (TerritoryID);
