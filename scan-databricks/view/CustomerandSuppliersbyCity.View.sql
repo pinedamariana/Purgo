d0eed30b05ea0670d1ea4d0766f99ef5ae645353
Purgo AI <support@purgo.ai>
2024-08-29 10:40:44 -0500

    Commit message

--- scan-databricks/view/CustomerandSuppliersbyCity.View.sql
+++ scan-databricks/view/CustomerandSuppliersbyCity.View.sql
@@ -1,8 +1,7 @@
 
 CREATE OR REPLACE VIEW `Customer and Suppliers by City` AS
-SELECT City, CompanyName, ContactName, 'Customers' AS Relationship
+SELECT City, CompanyName, ContactName, 'Customers' AS Relationship 
 FROM Customers
-UNION ALL
-SELECT City, CompanyName, ContactName, 'Suppliers' AS Relationship
+UNION SELECT City, CompanyName, ContactName, 'Suppliers' AS Relationship
 FROM Suppliers
--- ORDER BY City, CompanyName
+
