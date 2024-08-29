e1728bec868736bf94a3c47f175508ed8fd68c42
Purgo AI <support@purgo.ai>
2024-08-29 10:41:03 -0500

    Commit message

--- scan-databricks/view/ProductSalesfor1997.View.sql
+++ scan-databricks/view/ProductSalesfor1997.View.sql
@@ -1,16 +1,19 @@
 
-CREATE VIEW `Product Sales for 1997` AS
-SELECT 
-  Cateries.CateryName, 
-  Products.ProductName, 
-  SUM(CAST("Order Details".UnitPrice * Quantity * (1 - Discount) AS DECIMAL(10,2))) AS ProductSales
-FROM 
-  Cateries 
-  INNER JOIN Products ON Cateries.CateryID = Products.CateryID
-  INNER JOIN Orders ON Orders.OrderID = "Order Details".OrderID
-  INNER JOIN "Order Details" ON Products.ProductID = "Order Details".ProductID
-WHERE 
-  Orders.ShippedDate BETWEEN '1997-01-01' AND '1997-12-31'
-GROUP BY 
-  Cateries.CateryName, 
-  Products.ProductName;
+CREATE OR REPLACE VIEW `Product Sales for 1997` AS
+SELECT
+    Cateries.CateryName,
+    Products.ProductName,
+    SUM(CAST("Order Details".UnitPrice * Quantity * (1 - Discount) AS DECIMAL(19,4))) AS ProductSales
+FROM
+    Cateries
+JOIN
+    Products ON Cateries.CateryID = Products.CateryID
+JOIN
+    Orders ON Orders.OrderID = "Order Details".OrderID
+JOIN
+    "Order Details" ON Products.ProductID = "Order Details".ProductID
+WHERE
+    Orders.ShippedDate BETWEEN '1997-01-01' AND '1997-12-31'
+GROUP BY
+    Cateries.CateryName,
+    Products.ProductName
