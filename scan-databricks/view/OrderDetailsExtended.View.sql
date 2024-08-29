2f519fceb4c0a7b59cded58cdcc2fc249aa4e325
Purgo AI <support@purgo.ai>
2024-08-28 19:30:59 -0500

    Commit message

---
+++ scan-databricks/view/OrderDetailsExtended.View.sql
@@ -0,0 +1,11 @@
+
+CREATE OR REPLACE VIEW `Order Details Extended` AS
+SELECT `Order Details`.OrderID, 
+       `Order Details`.ProductID, 
+       Products.ProductName, 
+       `Order Details`.UnitPrice, 
+       `Order Details`.Quantity, 
+       `Order Details`.Discount, 
+       (CAST((`Order Details`.UnitPrice * Quantity * (1 - Discount)) AS DECIMAL(19, 4))) AS ExtendedPrice
+FROM Products 
+INNER JOIN `Order Details` ON Products.ProductID = `Order Details`.ProductID;
