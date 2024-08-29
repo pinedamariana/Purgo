752a085f478442c2bc98a6978a83ecfdab94c416
Purgo AI <support@purgo.ai>
2024-08-29 10:40:05 -0500

    Commit message

--- scan-databricks/table/Cateries.Table.sql
+++ scan-databricks/table/Cateries.Table.sql
@@ -1,9 +1,7 @@
-
 CREATE TABLE Cateries (
   CateryID INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
   CateryName STRING NOT NULL,
-  Description STRING,
-  Picture BINARY,
+  Description STRING NULL,
+  Picture BINARY NULL,
   PRIMARY KEY (CateryID)
 )
-
