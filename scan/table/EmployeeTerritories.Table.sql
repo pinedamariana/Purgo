9f247edbc4c148993ce0207f75e01f2a79ad8d96
Purgo AI <support@purgo.ai>
2024-08-28 19:04:54 -0500

    Commit message

---
+++ scan/table/EmployeeTerritories.Table.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE [EmployeeTerritories] (
+	[EmployeeID] INTEGER NOT NULL, 
+	[TerritoryID] NVARCHAR(20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL, 
+	CONSTRAINT [PK_EmployeeTerritories] PRIMARY KEY ([EmployeeID], [TerritoryID]), 
+	CONSTRAINT [FK_EmployeeTerritories_Employees] FOREIGN KEY([EmployeeID]) REFERENCES [Employees] ([EmployeeID]), 
+	CONSTRAINT [FK_EmployeeTerritories_Territories] FOREIGN KEY([TerritoryID]) REFERENCES [Territories] ([TerritoryID])
+)
+
