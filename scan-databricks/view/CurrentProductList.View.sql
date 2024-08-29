ed7cd3aa80d64e4032cb9193a41c00dc5748894e
Purgo AI <support@purgo.ai>
2024-08-29 10:40:43 -0500

    Commit message

--- scan-databricks/view/CurrentProductList.View.sql
+++ scan-databricks/view/CurrentProductList.View.sql
@@ -1,5 +1,6 @@
 
-CREATE VIEW `Current Product List` AS
+CREATE OR REPLACE VIEW `Current Product List` AS
 SELECT Product_List.ProductID, Product_List.ProductName
 FROM Products AS Product_List
 WHERE Product_List.Discontinued = 0;
+-- ORDER BY Product_List.ProductName;
