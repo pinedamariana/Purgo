3e1b58328bc6b11d4b7e3d4398ea775864a9d701
Purgo AI <support@purgo.ai>
2024-08-28 19:35:09 -0500

    Commit message

---
+++ scan-migration/export/Employees.Table-export.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import pyodbc
+import pandas as pd
+from sqlalchemy import create_engine
+
+sqlServerHost = os.getenv("PURGO_SQL_SERVER_HOST")
+if not sqlServerHost:
+    sys.exit("PURGO_SQL_SERVER_HOST env variable not set")
+
+sqlServerPort = os.getenv("PURGO_SQL_SERVER_PORT")
+if not sqlServerPort:
+    sys.exit("PURGO_SQL_SERVER_PORT env variable not set")
+
+sqlServerDB = os.getenv("PURGO_SQL_SERVER_DB")
+if not sqlServerDB:
+    sys.exit("PURGO_SQL_SERVER_DB env variable not set")
+
+sqlServerUser = os.getenv("PURGO_SQL_SERVER_USER")
+if not sqlServerUser:
+    sys.exit("PURGO_SQL_SERVER_USER env variable not set")
+
+sqlServerPassword = os.getenv("PURGO_SQL_SERVER_PASSWORD")
+if not sqlServerPassword:
+    sys.exit("PURGO_SQL_SERVER_PASSWORD env variable not set")
+
+sqlServerConnection = "Server=tcp:"+ sqlServerHost + "," + sqlServerPort + ";Database={" + sqlServerDB + "};Uid={" + sqlServerUser + "};Pwd={" + sqlServerPassword + "};Encrypt=yes;TrustServerCertificate=yes;Connection Timeout=30;"
+sqlServerEngine = create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};" + sqlServerConnection)
+
+df = pd.read_sql('select * from Employees', sqlServerEngine)
+df.to_parquet("Employees.parquet", index=True)
