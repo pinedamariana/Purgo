dc51d44e1437bd7e1c1373fc40549ed5fa49592b
Purgo AI <support@purgo.ai>
2024-08-28 19:30:23 -0500

    Commit message

---
+++ scan-databricks/table/Customers.Table.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE Customers (
+    CustomerID STRING NOT NULL, 
+    CompanyName STRING NOT NULL, 
+    ContactName STRING, 
+    ContactTitle STRING, 
+    Address STRING, 
+    City STRING, 
+    Region STRING, 
+    PostalCode STRING, 
+    Country STRING, 
+    Phone STRING, 
+    Fax STRING,
+    PRIMARY KEY (CustomerID)
+)
